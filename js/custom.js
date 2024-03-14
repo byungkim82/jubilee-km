ef5fb504e6293d027b47f5a4fe9e57b915d13a18
Jony Jeon (Bora Jeon) <jonyjeon26@gmail.com>
2024-03-13 21:49:28 -0400

    Add responsive layout for Landing page

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,17 @@
 $(function(){
-     $(window).scroll(function () {
-       if ($(window).scrollTop() > 50) { $('.goto-top').addClass('active') }
-       else { $('.goto-top').removeClass('active') }
- })
+
+  var mediaQuery = window.matchMedia('(max-width: 768px)')
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 50) { $('.goto-top').addClass('active') }
+      else { $('.goto-top').removeClass('active') } })
+
+    $('.trigger').click(function(){
+    $(this).toggleClass('active')
+    $('.gnb > ul').toggleClass('active')
+    $('.goyoutube').toggleClass('active')
+  })
+    $('.gnb a').click(function(){
+    $('.gnb, .trigger').removeClass('active')
+  })
 });
