13c259daf7abb1d79981639ac05c69f5c0a5155e
Byung Joo Kim <byungkim82@gmail.com>
2024-03-16 14:04:00 -0400

    Replace scripts and styles.

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  // Add scroll event listener
+  window.addEventListener('scroll', function() {
+    document.querySelector('.goto-top').classList.toggle('active', window.scrollY > 50);
+  });
+
+  // Add trigger event listener
+  document.querySelector(".trigger").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".gnb > ul").classList.toggle("active");
+    document.querySelector(".goyoutube").classList.toggle("active");
+  });
+
+  // Add gnb a event listener
+  document.querySelectorAll('.gnb a').forEach(function(item) {
+    item.addEventListener('click', function() {
+      document.querySelector('.gnb').classList.remove('active');
+      document.querySelector('.trigger').classList.remove('active');
+    });
+  });
+
+});
